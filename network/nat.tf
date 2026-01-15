693b3bb5eb4059ee0e3925569fe8350d29605a49
Yevgeni Shapiro <23049337+yevgenis1980@users.noreply.github.com>
2026-01-15 13:44:01 +0200

    Update nat.tf

--- network/nat.tf
+++ network/nat.tf
@@ -1,14 +1,10 @@
 
 resource "aws_eip" "nat" {
-domain = "vpc"
+  domain = "vpc"
 }
 
 resource "aws_nat_gateway" "this" {
-allocation_id = aws_eip.nat.id
-subnet_id = aws_subnet.public[0].id
-
-
-tags = {
-Name = "${var.project_name}-nat"
-}
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+  tags          = { Name = "${var.project_name}-nat" }
 }
