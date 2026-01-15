ec2925c39ef64b2e4508487a15359c011f0d83ba
Yevgeni Shapiro <23049337+yevgenis1980@users.noreply.github.com>
2026-01-15 12:34:55 +0200

    Update s3.tf

--- frontend/s3.tf
+++ frontend/s3.tf
@@ -1,1 +1,11 @@
 
+resource "aws_s3_bucket" "frontend" {
+bucket = "${var.project_name}-frontend-${random_id.suffix.hex}"
+acl = "public-read"
+
+
+website {
+index_document = "index.html"
+error_document = "index.html"
+}
+}
