5dd4d993a6d3de592344cc1186f8295ba1e79ed0
Yevgeni Shapiro <23049337+yevgenis1980@users.noreply.github.com>
2026-01-15 13:43:31 +0200

    Update subnets.tf

--- network/subnets.tf
+++ network/subnets.tf
@@ -1,29 +1,20 @@
+data "aws_availability_zones" "available" {}
 
 resource "aws_subnet" "public" {
-count = length(var.public_subnets)
+  count                   = length(var.public_subnets)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnets[count.index]
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
 
-vpc_id = aws_vpc.this.id
-cidr_block = var.public_subnets[count.index]
-map_public_ip_on_launch = true
-availability_zone = data.aws_availability_zones.available.names[count.index]
-
-tags = {
-Name = "${var.project_name}-public-${count.index}"
+  tags = { Name = "${var.project_name}-public-${count.index}" }
 }
-}
-
 
 resource "aws_subnet" "private" {
-count = length(var.private_subnets)
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
 
-vpc_id = aws_vpc.this.id
-cidr_block = var.private_subnets[count.index]
-availability_zone = data.aws_availability_zones.available.names[count.index]
-
-
-tags = {
-Name = "${var.project_name}-private-${count.index}"
+  tags = { Name = "${var.project_name}-private-${count.index}" }
 }
-}
-
-data "aws_availability_zones" "available" {}
