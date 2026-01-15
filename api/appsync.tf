c782abf6c9696828c00f655760678c797a9c0649
Yevgeni Shapiro <23049337+yevgenis1980@users.noreply.github.com>
2026-01-15 12:15:49 +0200

    Update appsync.tf

--- api/appsync.tf
+++ api/appsync.tf
@@ -1,1 +1,10 @@
 
+resource "aws_appsync_graphql_api" "chat_api" {
+name = "${var.project_name}-api"
+authentication_type = "API_KEY"
+}
+
+
+resource "aws_appsync_api_key" "key" {
+api_id = aws_appsync_graphql_api.chat_api.id
+}
