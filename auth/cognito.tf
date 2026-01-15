b04dddd36630e43d1388343e8ac0bb6fdbbd4edb
Yevgeni Shapiro <23049337+yevgenis1980@users.noreply.github.com>
2026-01-15 13:39:36 +0200

    update

--- auth/cognito.tf
+++ auth/cognito.tf
@@ -1,62 +1,30 @@
-
 resource "aws_cognito_user_pool" "this" {
-name = "${var.project_name}-users"
+  name = "${var.project_name}-users"
 
+  auto_verified_attributes = ["email"]
 
-auto_verified_attributes = ["email"]
-
-
-password_policy {
-minimum_length = 8
-require_lowercase = true
-require_numbers = true
-require_symbols = true
-require_uppercase = true
+  password_policy {
+    minimum_length    = 8
+    require_lowercase = true
+    require_numbers   = true
+    require_symbols   = true
+    require_uppercase = true
+  }
 }
-}
-
 
 resource "aws_cognito_user_pool_client" "client" {
-name = "${var.project_name}-client"
-user_pool_id = aws_cognito_user_pool.this.id
-
-generate_secret = false
-
-explicit_auth_flows = [
-"ALLOW_USER_PASSWORD_AUTH",
-"ALLOW_REFRESH_TOKEN_AUTH",
-"ALLOW_USER_SRP_AUTH"
-]
+  name         = "${var.project_name}-client"
+  user_pool_id = aws_cognito_user_pool.this.id
+  generate_secret = false
+  explicit_auth_flows = ["ALLOW_USER_PASSWORD_AUTH", "ALLOW_REFRESH_TOKEN_AUTH", "ALLOW_USER_SRP_AUTH"]
 }
-
 
 resource "aws_cognito_identity_pool" "this" {
-identity_pool_name = "${var.project_name}-identity"
-allow_unauthenticated_identities = false
+  identity_pool_name                = "${var.project_name}-identity"
+  allow_unauthenticated_identities = false
 
-cognito_identity_providers {
-client_id = aws_cognito_user_pool_client.client.id
-provider_name = aws_cognito_user_pool.this.endpoint
-server_side_token_check = false
- }
+  cognito_identity_providers {
+    client_id     = aws_cognito_user_pool_client.client.id
+    provider_name = aws_cognito_user_pool.this.endpoint
+  }
 }
-
-resource "aws_iam_role" "authenticated" {
-name = "${var.project_name}-authenticated"
-
-
-assume_role_policy = jsonencode({
-Version = "2012-10-17"
-Statement = [{
-Effect = "Allow"
-Principal = {
-Federated = "cognito-identity.amazonaws.com"
-}
-Action = "sts:AssumeRoleWithWebIdentity"
-Condition = {
-StringEquals = {
-"cognito-identity.amazonaws.com:aud" = aws_cognito_identity_pool.this.id
-}
-}
-}]
-})
