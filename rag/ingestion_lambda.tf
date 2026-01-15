7884a15d43e0a3e86fd3fa2c380b870190ebfcb4
Yevgeni Shapiro <23049337+yevgenis1980@users.noreply.github.com>
2026-01-15 12:21:23 +0200

    Update ingestion_lambda.tf

--- rag/ingestion_lambda.tf
+++ rag/ingestion_lambda.tf
@@ -1,1 +1,17 @@
 
+resource "aws_lambda_function" "ingest" {
+function_name = "${var.project_name}-ingest"
+role = module.iam.lambda_role_arn
+handler = "handler.main"
+runtime = "python3.11"
+
+
+filename = "lambda/ingest.zip"
+
+
+environment {
+variables = {
+BUCKET = aws_s3_bucket.documents.bucket
+COLLECTION = aws_opensearchserverless_collection.vector.name
+}
+}
