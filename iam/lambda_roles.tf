b70bc836f06cb39e72ac5c1bc001caf0386dab07
Yevgeni Shapiro <23049337+yevgenis1980@users.noreply.github.com>
2026-01-15 13:42:00 +0200

    update

--- iam/lambda_roles.tf
+++ iam/lambda_roles.tf
@@ -1,50 +1,18 @@
 
 resource "aws_iam_role" "lambda_role" {
-name = "${var.project_name}-lambda-role"
+  name = "${var.project_name}-lambda-role"
 
-
-assume_role_policy = jsonencode({
-Version = "2012-10-17"
-Statement = [
-{
-Effect = "Allow"
-Principal = {
-Service = "lambda.amazonaws.com"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
 }
-Action = "sts:AssumeRole"
-}
-]
-})
-}
-
 
 resource "aws_iam_role_policy_attachment" "basic_logs" {
-role = aws_iam_role.lambda_role.name
-policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-
-resource "aws_iam_policy" "dynamo_access" {
-name = "${var.project_name}-dynamo-access"
-
-
-policy = jsonencode({
-Version = "2012-10-17"
-Statement = [
-{
-Effect = "Allow"
-Action = [
-"dynamodb:PutItem",
-"dynamodb:GetItem",
-"dynamodb:Query"
-]
-Resource = aws_dynamodb_table.chat_history.arn
-}
-]
-})
-}
-
-resource "aws_iam_role_policy_attachment" "dynamo_attach" {
-role = aws_iam_role.lambda_role.name
-policy_arn = aws_iam_policy.dynamo_access.arn
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
